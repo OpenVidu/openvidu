8a8907323989171b6ec0a2c87cdb6d0b27602c29
pabloFuente <pablofuenteperez@gmail.com>
2017-05-31 10:41:59 +0200

    Angular project relocated into /src

--- openvidu-sample-app/src/angular/BuildFrontWarAndRun.sh
+++ openvidu-sample-app/src/angular/BuildFrontWarAndRun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd frontend
-ng build --output-path ../static
-cd ../../../../
+ng build --output-path ../../main/resources/static
+cd ../../../
 mvn clean package
 java -jar target/openvidu-sample-app-0.0.1-SNAPSHOT.war
