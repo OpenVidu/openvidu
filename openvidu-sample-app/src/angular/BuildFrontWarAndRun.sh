9e90367297e857e81708f41eb8404fae60e3c607
pabloFuente <pablofuenteperez@gmail.com>
2017-07-12 10:17:45 +0200

    openvidu-sample-app moved to its own repo (OpenVidu/classroom-demo)

--- openvidu-sample-app/src/angular/BuildFrontWarAndRun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd frontend
-ng build --output-path ../../main/resources/static
-cd ../../../
-mvn clean package
-java -jar target/openvidu-sample-app-0.0.1-SNAPSHOT.war
--- openvidu-sample-app/src/main/docker/wrapper.sh
+++
@@ -1,2 +0,0 @@
-sleep 40
-java -Djava.security.egd=file:/dev/./urandom -jar /openvidu-sample-app.war
