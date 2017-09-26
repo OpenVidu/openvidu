64359cd8ef6b2d8cca47d481917b349b8009cbb0
pabloFuente <pablofuenteperez@gmail.com>
2017-09-26 11:32:23 +0200

    openvidu-browser Maven project removed (now only NPM project)

--- update-plainjs-prod.sh
+++ update-plainjs-prod.sh
@@ -1,19 +1,19 @@
 VERSION="$1"
 
 # openvidu-insecure-js
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-insecure-js/web/openvidu-browser-"$1".min.js
+cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-insecure-js/web/openvidu-browser-"$1".min.js
 
 # openvidu-js-java
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-js-java/src/main/resources/static/openvidu-browser-"$1".min.js
+cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-js-java/src/main/resources/static/openvidu-browser-"$1".min.js
 
 # openvidu-mvc-java
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-mvc-java/src/main/resources/static/openvidu-browser-"$1".min.js
+cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-mvc-java/src/main/resources/static/openvidu-browser-"$1".min.js
 
 # openvidu-js-node
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-js-node/public/openvidu-browser-"$1".min.js
+cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-js-node/public/openvidu-browser-"$1".min.js
 
 # openvidu-mvc-node
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-mvc-node/public/openvidu-browser-"$1".min.js
+cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-mvc-node/public/openvidu-browser-"$1".min.js
 
 # openvidu-getaroom
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-getaroom/web/openvidu-browser-"$1".min.js
+cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-getaroom/web/openvidu-browser-"$1".min.js
--- update-plainjs.sh
+++ update-plainjs.sh
@@ -1,22 +1,22 @@
 VERSION="$1"
 
 # openvidu-hello-world
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-hello-world/web/openvidu-browser-"$1".js
+cp openvidu-browser/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-hello-world/web/openvidu-browser-"$1".js
 
 # openvidu-insecure-js
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-insecure-js/web/openvidu-browser-"$1".js
+cp openvidu-browser/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-insecure-js/web/openvidu-browser-"$1".js
 
 # openvidu-js-java
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-js-java/src/main/resources/static/openvidu-browser-"$1".js
+cp openvidu-browser/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-js-java/src/main/resources/static/openvidu-browser-"$1".js
 
 # openvidu-mvc-java
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-mvc-java/src/main/resources/static/openvidu-browser-"$1".js
+cp openvidu-browser/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-mvc-java/src/main/resources/static/openvidu-browser-"$1".js
 
 # openvidu-js-node
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-js-node/public/openvidu-browser-"$1".js
+cp openvidu-browser/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-js-node/public/openvidu-browser-"$1".js
 
 # openvidu-mvc-node
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-mvc-node/public/openvidu-browser-"$1".js
+cp openvidu-browser/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-mvc-node/public/openvidu-browser-"$1".js
 
 # openvidu-getaroom
-cp openvidu-browser/src/main/resources/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-getaroom/web/openvidu-browser-"$1".js
+cp openvidu-browser/static/js/openvidu-browser-"$1".js ../openvidu-tutorials/openvidu-getaroom/web/openvidu-browser-"$1".js
