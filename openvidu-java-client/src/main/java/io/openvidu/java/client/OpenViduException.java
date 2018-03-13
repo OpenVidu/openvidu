d52991800012aa73af9bed96df570093b60b27ea
pabloFuente <pablofuenteperez@gmail.com>
2018-03-13 16:14:37 +0100

    openvidu-java-client recording support

--- openvidu-java-client/src/main/java/io/openvidu/java/client/OpenViduException.java
+++ openvidu-java-client/src/main/java/io/openvidu/java/client/OpenViduException.java
@@ -18,60 +18,64 @@
 package io.openvidu.java.client;
 
 public class OpenViduException extends RuntimeException {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  public static enum Code {
-    GENERIC_ERROR_CODE(999),
+	public static enum Code {
+		GENERIC_ERROR_CODE(999),
 
-    TRANSPORT_ERROR_CODE(803), TRANSPORT_RESPONSE_ERROR_CODE(802), TRANSPORT_REQUEST_ERROR_CODE(
-        801),
+		TRANSPORT_ERROR_CODE(803), TRANSPORT_RESPONSE_ERROR_CODE(802), TRANSPORT_REQUEST_ERROR_CODE(801),
 
-    MEDIA_MUTE_ERROR_CODE(307), MEDIA_NOT_A_WEB_ENDPOINT_ERROR_CODE(
-        306), MEDIA_RTP_ENDPOINT_ERROR_CODE(305), MEDIA_WEBRTC_ENDPOINT_ERROR_CODE(
-            304), MEDIA_ENDPOINT_ERROR_CODE(
-                303), MEDIA_SDP_ERROR_CODE(302), MEDIA_GENERIC_ERROR_CODE(301),
+		MEDIA_MUTE_ERROR_CODE(307), MEDIA_NOT_A_WEB_ENDPOINT_ERROR_CODE(306), MEDIA_RTP_ENDPOINT_ERROR_CODE(
+				305), MEDIA_WEBRTC_ENDPOINT_ERROR_CODE(
+						304), MEDIA_ENDPOINT_ERROR_CODE(303), MEDIA_SDP_ERROR_CODE(302), MEDIA_GENERIC_ERROR_CODE(301),
 
-    ROOM_CANNOT_BE_CREATED_ERROR_CODE(204), ROOM_CLOSED_ERROR_CODE(203), ROOM_NOT_FOUND_ERROR_CODE(
-        202), ROOM_GENERIC_ERROR_CODE(201),
+		ROOM_CANNOT_BE_CREATED_ERROR_CODE(204), ROOM_CLOSED_ERROR_CODE(203), ROOM_NOT_FOUND_ERROR_CODE(
+				202), ROOM_GENERIC_ERROR_CODE(201),
 
-    USER_NOT_STREAMING_ERROR_CODE(105), EXISTING_USER_IN_ROOM_ERROR_CODE(
-        104), USER_CLOSED_ERROR_CODE(
-            103), USER_NOT_FOUND_ERROR_CODE(102), USER_GENERIC_ERROR_CODE(101),
-    
-    USER_UNAUTHORIZED_ERROR_CODE(401), ROLE_NOT_FOUND_ERROR_CODE(402),
-    	SESSIONID_CANNOT_BE_CREATED_ERROR_CODE(403), TOKEN_CANNOT_BE_CREATED_ERROR_CODE(404),
-    	
-    USER_METADATA_FORMAT_INVALID_ERROR_CODE(500);
+		USER_NOT_STREAMING_ERROR_CODE(105), EXISTING_USER_IN_ROOM_ERROR_CODE(104), USER_CLOSED_ERROR_CODE(
+				103), USER_NOT_FOUND_ERROR_CODE(102), USER_GENERIC_ERROR_CODE(101),
 
-    private int value;
+		USER_UNAUTHORIZED_ERROR_CODE(401), ROLE_NOT_FOUND_ERROR_CODE(402), SESSIONID_CANNOT_BE_CREATED_ERROR_CODE(
+				403), TOKEN_CANNOT_BE_CREATED_ERROR_CODE(404),
 
-    private Code(int value) {
-      this.value = value;
-    }
+		USER_METADATA_FORMAT_INVALID_ERROR_CODE(500),
 
-    public int getValue() {
-      return this.value;
-    }
-  }
+		SIGNAL_FORMAT_INVALID_ERROR_CODE(600), SIGNAL_TO_INVALID_ERROR_CODE(601), SIGNAL_MESSAGE_INVALID_ERROR_CODE(
+				602),
 
-  private Code code = Code.GENERIC_ERROR_CODE;
+		RECORDING_FILE_EMPTY_ERROR(707), RECORDING_DELETE_ERROR_CODE(706), RECORDING_LIST_ERROR_CODE(
+				705), RECORDING_STOP_ERROR_CODE(704), RECORDING_START_ERROR_CODE(
+						703), RECORDING_REPORT_ERROR_CODE(702), RECORDING_COMPLETION_ERROR_CODE(701);
 
-  public OpenViduException(Code code, String message) {
-    super(message);
-    this.code = code;
-  }
+		private int value;
 
-  public Code getCode() {
-    return code;
-  }
+		private Code(int value) {
+			this.value = value;
+		}
 
-  public int getCodeValue() {
-    return code.getValue();
-  }
+		public int getValue() {
+			return this.value;
+		}
+	}
 
-  @Override
-  public String toString() {
-    return "Code: " + getCodeValue() + " " + super.toString();
-  }
+	private Code code = Code.GENERIC_ERROR_CODE;
+
+	public OpenViduException(Code code, String message) {
+		super(message);
+		this.code = code;
+	}
+
+	public Code getCode() {
+		return code;
+	}
+
+	public int getCodeValue() {
+		return code.getValue();
+	}
+
+	@Override
+	public String toString() {
+		return "Code: " + getCodeValue() + " " + super.toString();
+	}
 
 }
--- openvidu-java-client/src/main/java/io/openvidu/java/client/Session.java
+++ openvidu-java-client/src/main/java/io/openvidu/java/client/Session.java
@@ -1,16 +1,11 @@
 package io.openvidu.java.client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import io.openvidu.java.client.OpenViduException.Code;
 
@@ -20,6 +15,9 @@ public class Session {
 	private String urlOpenViduServer;
 	private String sessionId;
 	private SessionProperties properties;
+	
+	final static String API_SESSIONS = "api/sessions";
+	final static String API_TOKENS = "api/tokens";
 
 	protected Session(HttpClient httpClient, String urlOpenViduServer) throws OpenViduException {
 		this.httpClient = httpClient;
@@ -35,6 +33,7 @@ public class Session {
 		this.sessionId = this.getSessionId();
 	}
 
+	@SuppressWarnings("unchecked")
 	public String getSessionId() throws OpenViduException {
 
 		if (this.hasSessionId()) {
@@ -42,15 +41,15 @@ public class Session {
 		}
 
 		try {
+			HttpPost request = new HttpPost(this.urlOpenViduServer + API_SESSIONS);
 			
 			JSONObject json = new JSONObject();
-			json.put("archiveMode", properties.archiveMode().name());
 			json.put("archiveLayout", properties.archiveLayout().name());
+			json.put("archiveMode", properties.archiveMode().name());
 			json.put("mediaMode", properties.mediaMode().name());
-
-			HttpPost request = new HttpPost(this.urlOpenViduServer + "api/sessions");
 			StringEntity params = new StringEntity(json.toString());
-			request.addHeader("content-type", "application/json");
+			
+			request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 			request.setEntity(params);
 			
 			HttpResponse response = httpClient.execute(request);
@@ -58,7 +57,7 @@ public class Session {
 			if ((statusCode == org.apache.http.HttpStatus.SC_OK)) {
 				System.out.println("Returning a SESSIONID");
 				String id = "";
-				id = this.httpResponseToString(response);
+				id = (String) OpenVidu.httpResponseToJson(response).get("id");
 				this.sessionId = id;
 				return id;
 			} else {
@@ -75,6 +74,7 @@ public class Session {
 		return this.generateToken(new TokenOptions.Builder().role(OpenViduRole.PUBLISHER).build());
 	}
 
+	@SuppressWarnings("unchecked")
 	public String generateToken(TokenOptions tokenOptions) throws OpenViduException {
 
 		if (!this.hasSessionId()) {
@@ -82,15 +82,15 @@ public class Session {
 		}
 
 		try {
-
+			HttpPost request = new HttpPost(this.urlOpenViduServer + API_TOKENS);
+			
 			JSONObject json = new JSONObject();
 			json.put("session", this.sessionId);
 			json.put("role", tokenOptions.getRole().name());
 			json.put("data", tokenOptions.getData());
-
-			HttpPost request = new HttpPost(this.urlOpenViduServer + "api/tokens");
 			StringEntity params = new StringEntity(json.toString());
-			request.addHeader("content-type", "application/json");
+			
+			request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 			request.setEntity(params);
 
 			HttpResponse response = httpClient.execute(request);
@@ -98,7 +98,7 @@ public class Session {
 			int statusCode = response.getStatusLine().getStatusCode();
 			if ((statusCode == org.apache.http.HttpStatus.SC_OK)) {
 				System.out.println("Returning a TOKEN");
-				return this.httpResponseToString(response);
+				return (String) OpenVidu.httpResponseToJson(response).get("id");
 			} else {
 				throw new OpenViduException(Code.TOKEN_CANNOT_BE_CREATED_ERROR_CODE, Integer.toString(statusCode));
 			}
@@ -116,17 +116,6 @@ public class Session {
 	@Override
 	public String toString() {
 		return this.sessionId;
-	}
-
-	private String httpResponseToString(HttpResponse response) throws IOException, ParseException {
-		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-		StringBuffer buf = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			buf.append(line);
-		}
-		JSONParser parser = new JSONParser();
-		return ((String) ((JSONObject) parser.parse(buf.toString())).get("id"));
 	}
 
 	private boolean hasSessionId() {
