e9089eeb82d095f5ce4fe352ced97fffa9f2c66a
pabloFuente <pablofuenteperez@gmail.com>
2018-04-18 15:18:12 +0200

    openvidu-server-kms bug fix (KMS_TURN_URL was being overrided)

--- openvidu-server/docker/openvidu-server-kms/kms.sh
+++ openvidu-server/docker/openvidu-server-kms/kms.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -x
 set -e
 
-if [ -n "$KMS_TURN_URL" ]; then
-  echo "turnURL=$KMS_TURN_URL" > /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-fi
-
 if [ -n "$KMS_STUN_IP" -a -n "$KMS_STUN_PORT" ]; then
   # Generate WebRtcEndpoint configuration
   echo "stunServerAddress=$KMS_STUN_IP" > /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
   echo "stunServerPort=$KMS_STUN_PORT" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+  if [ -n "$KMS_TURN_URL" ]; then
+    echo "turnURL=$KMS_TURN_URL" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+  fi
+elif [ -n "$KMS_TURN_URL" ]; then
+    echo "turnURL=$KMS_TURN_URL" > /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 fi
 
 # Remove ipv6 local loop until ipv6 is supported
