fadf19cfb046c7b209e9d44ebba6f9fa0c330723
pabloFuente <pablofuenteperez@gmail.com>
2018-04-23 11:07:31 +0200

    openvidu-node-client: tslint and typedoc

---
+++ openvidu-node-client/config/typedoc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    lib: [
+        "lib.dom.d.ts",
+        "lib.es5.d.ts",
+        "lib.es2015.promise.d.ts",
+        "lib.scripthost.d.ts"
+    ],
+    mode: "file",
+    module: "commonjs",
+    name: "OpenVidu Browser",
+    target: "es5",
+    externalPattern: "node_modules",
+    exclude: [
+        "**/OpenViduInternal/Interfaces/Private/**",
+        "**/OpenViduInternal/WebRtcStats/WebRtcStats.ts",
+        "**/OpenViduInternal/VersionAdapter.ts"
+    ],
+    excludeExternals: true,
+    excludePrivate: true,
+    theme: "default",
+    plugin: [
+        "typedoc-plugin-sourcefile-url",
+    ],
+    'sourcefile-url-prefix': "https://github.com/OpenVidu/openvidu/tree/master/openvidu-browser/src/"
+}
