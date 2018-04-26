93b6c33e2b9435f48cd24f311a6f2407b5c70bab
pabloFuente <pablofuenteperez@gmail.com>
2018-04-26 17:47:40 +0200

    openvidu-node-client TypeDoc custom theme

--- openvidu-node-client/config/typedoc/typedoc.js
+++ openvidu-node-client/config/typedoc/typedoc.js
@@ -7,15 +7,20 @@ module.exports = {
     ],
     mode: "file",
     module: "commonjs",
-    name: "OpenVidu Node Client",
+    name: "OpenVidu Browser",
     target: "es5",
     externalPattern: "node_modules",
+    exclude: [
+        "**/OpenViduInternal/Interfaces/Private/**",
+        "**/OpenViduInternal/WebRtcStats/WebRtcStats.ts",
+        "**/OpenViduInternal/VersionAdapter.ts"
+    ],
     excludeExternals: true,
     excludePrivate: true,
-    theme: "default",
+    theme: "./config/typedoc/custom-theme/bin",
     plugin: [
         "typedoc-plugin-sourcefile-url",
     ],
-    'sourcefile-url-prefix': "https://github.com/OpenVidu/openvidu/tree/master/openvidu-node-client/src/",
+    'sourcefile-url-prefix': "https://github.com/OpenVidu/openvidu/tree/master/openvidu-browser/src/",
     readme: "none"
 }
