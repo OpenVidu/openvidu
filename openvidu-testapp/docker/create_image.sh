9eae6974afc91f99716949bcab2b9556315928e1
pabloFuente <pablofuenteperez@gmail.com>
2018-06-04 15:57:44 +0200

    openvidu-testapp Docker image recording support (front served with http-server)

--- openvidu-testapp/docker/create_image.sh
+++ openvidu-testapp/docker/create_image.sh
@@ -5,7 +5,12 @@ cp ../../openvidu-server/target/openvidu-server-"$1".jar ./openvidu-server.jar
 cd ../
 ng build --prod
 cp -a dist/. ./docker/web/
-cd docker
+cd docker/web
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj '/CN=www.mydom.com/O=My Company LTD./C=US' -keyout key.pem -out cert.pem
+openssl pkcs12 -export -in cert.pem -inkey key.pem -out keystore.p12 -password pass:CERT_PASS -name CERT_ALIAS -CAfile cert.pem
+keytool -importkeystore -srckeystore keystore.p12 -srcstoretype PKCS12 -deststorepass CERT_PASS -srcstorepass CERT_PASS -destkeystore NEW.jks -deststoretype JKS
+cd ..
+
 
 # Build docker image
 docker build -t openvidu/testapp .
