f0984dc128c3156f4a9bccc554f46701cb427f43
pabloFuente <pablofuenteperez@gmail.com>
2018-12-11 15:07:36 +0100

    openvidu-server: info websocket buffer size limit increased

--- openvidu-server/src/main/java/io/openvidu/server/config/InfoSocketConfig.java
+++ openvidu-server/src/main/java/io/openvidu/server/config/InfoSocketConfig.java
@@ -22,19 +22,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
 public class InfoSocketConfig implements WebSocketConfigurer {
-	
-	@Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(infoHandler(), "/info").setAllowedOrigins("*");
-    }
 
-    @Bean
-    public InfoHandler infoHandler() {
-        return new InfoHandler();
-    }
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(infoHandler(), "/info").setAllowedOrigins("*");
+	}
+
+	@Bean
+	public InfoHandler infoHandler() {
+		return new InfoHandler();
+	}
+
+	@Bean
+	public ServletServerContainerFactoryBean createWebSocketContainer() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(50 * 1024);
+		return container;
+	}
 
 }
