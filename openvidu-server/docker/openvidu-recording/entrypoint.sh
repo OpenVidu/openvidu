204e05aed2178de322643cbd144267c19efeed89
pabloFuente <pablofuenteperez@gmail.com>
2019-04-03 11:31:19 +0200

    openvidu-recording container removed Java and selenium-server

--- openvidu-server/docker/openvidu-recording/entrypoint.sh
+++ openvidu-server/docker/openvidu-recording/entrypoint.sh
@@ -3,7 +3,7 @@
 ### Use container as a single headless chrome ###
 
 if [ "$HEADLESS_CHROME_ONLY" == true ]; then
-    java -jar -Dwebdriver.chrome.driver="$CHROME_DRIVER_PATH" /selenium-server-standalone.jar &> /selenium-server.log &
+    google-chrome --no-sandbox --headless --remote-debugging-port=$HEADLESS_CHROME_PORT  &> /chrome.log &
     sleep 100000000
 else
 
