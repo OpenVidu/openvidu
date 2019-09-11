77bad4e14f1b4522e21166cd7df6a8f978a9268c
pabloFuente <pablofuenteperez@gmail.com>
2019-09-11 10:33:10 +0200

    openvidu-node-client: avoid [object object] error on OpenVidu#fetch

--- openvidu-node-client/src/OpenVidu.ts
+++ openvidu-node-client/src/OpenVidu.ts
@@ -480,7 +480,7 @@ export class OpenVidu {
             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
             // http.ClientRequest in node.js
             console.error(error.request);
-            reject(new Error(error.request));
+            reject(error);
           } else {
             // Something happened in setting up the request that triggered an Error
             console.error('Error', error.message);
