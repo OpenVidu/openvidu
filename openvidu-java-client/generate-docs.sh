dcfa5f9268a25c6300c0936ddddbb91100fe2dca
pabloFuente <pablofuenteperez@gmail.com>
2020-03-21 23:30:14 +0100

    Librariy documentation scripts output to openvidu.io-docs

--- openvidu-java-client/generate-docs.sh
+++ openvidu-java-client/generate-docs.sh
@@ -2,7 +2,7 @@
 
 mvn javadoc:javadoc
 rm -rf ../../openvidu.io/api/openvidu-java-client/*
-cp -R ./target/site/apidocs/. ../../openvidu.io/api/openvidu-java-client
+cp -R ./target/site/apidocs/. ../../openvidu.io-docs/docs/api/openvidu-java-client
 
 # Add favicon to index.html
 sed -i -e 's/<head>/<head><link rel=\"shortcut icon\" href=\"\/img\/favicon.ico\" type=\"image\/x-icon\">/g' ../../openvidu.io/api/openvidu-java-client/index.html
