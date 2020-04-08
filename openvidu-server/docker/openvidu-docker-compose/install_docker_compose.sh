eeca207273846ea243b5a23e3669300b207d8ccc
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-04-08 15:51:20 +0200

    Update install docker-compose script

--- openvidu-server/docker/openvidu-docker-compose/install_docker_compose.sh
+++ openvidu-server/docker/openvidu-docker-compose/install_docker_compose.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 
+DOCKER_COMPOSE_FOLDER=openvidu-docker-compose
+
 # Create folder openvidu-docker-compose
-mkdir openvidu-docker-compose
+mkdir ${DOCKER_COMPOSE_FOLDER}
 
 # Download necessaries files
 curl https://raw.githubusercontent.com/OpenVidu/openvidu/master/openvidu-server/docker/openvidu-docker-compose/.env \
-     --output .env
+     --output ${DOCKER_COMPOSE_FOLDER}/.env
 curl https://raw.githubusercontent.com/OpenVidu/openvidu/master/openvidu-server/docker/openvidu-docker-compose/docker-compose.override.yml \
-     --output docker-compose.override.yml
+     --output ${DOCKER_COMPOSE_FOLDER}/docker-compose.override.yml
 curl https://raw.githubusercontent.com/OpenVidu/openvidu/master/openvidu-server/docker/openvidu-docker-compose/docker-compose.yml \
-     --output docker-compose.yml
+     --output ${DOCKER_COMPOSE_FOLDER}/docker-compose.yml
 curl https://raw.githubusercontent.com/OpenVidu/openvidu/master/openvidu-server/docker/openvidu-docker-compose/openvidu-restart.sh \
-    --output openvidu-restart.sh
+    --output ${DOCKER_COMPOSE_FOLDER}/openvidu-restart.sh
 curl https://raw.githubusercontent.com/OpenVidu/openvidu/master/openvidu-server/docker/openvidu-docker-compose/readme.md \
-    --output readme.md
+    --output ${DOCKER_COMPOSE_FOLDER}/readme.md
 
 # Add execution permissions
-chmod +x openvidu-restart.sh
+chmod +x ${DOCKER_COMPOSE_FOLDER}/openvidu-restart.sh
 
 # Create own certificated folder
-mkdir openvidu-docker-compose/owncert
+mkdir ${DOCKER_COMPOSE_FOLDER}/owncert
 
 # Ready to use
 printf "\n========================================"
 printf "\nOpenvidu CE has successfully installed."
-printf '\nNow run "./openvidu-restart.sh" for setup. Check "readme.md" for more details.\n\n'
+printf '\nNow run "./openvidu-restart.sh" in folder "openvidu-docker-compose" for setup.'
+printf '"Check "readme.md" in folder "openvidu-docker-compose" for more details.\n\n'
 exit 0
