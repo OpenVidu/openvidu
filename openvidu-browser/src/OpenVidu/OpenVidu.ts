4c944124390afde0ef29724f50f036a2d46a306d
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2020-04-17 17:34:54 +0200

    Correctly handle error on connection

--- openvidu-browser/src/OpenVidu/OpenVidu.ts
+++ openvidu-browser/src/OpenVidu/OpenVidu.ts
@@ -858,13 +858,14 @@ export class OpenVidu {
   /**
    * @hidden
    */
-  startWs(onConnectSucces: (error: Error) => void): void {
+  startWs(onConnectOrError: (error: Error) => void): void {
     const config = {
       heartbeat: 5000,
       sendCloseMessage: false,
       ws: {
         uri: this.wsUri,
-        onconnected: onConnectSucces,
+        onconnected: onConnectOrError,
+        onerror: onConnectOrError,
         ondisconnect: this.disconnectCallback.bind(this),
         onreconnecting: this.reconnectingCallback.bind(this),
         onreconnected: this.reconnectedCallback.bind(this)
