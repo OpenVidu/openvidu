68242da78fc178276a4f8829325c483ff7559708
pabloFuente <pablofuenteperez@gmail.com>
2020-04-21 13:49:10 +0200

    discover_my_public_ip.sh quotes fix

--- openvidu-server/docker/openvidu-server-pro/discover_my_public_ip.sh
+++ openvidu-server/docker/openvidu-server-pro/discover_my_public_ip.sh
@@ -9,7 +9,7 @@ function valid_ip()
     if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
         OIFS=$IFS
         IFS='.'
-        ip=("$ip")
+        ip=($ip)
         IFS=$OIFS
         [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
             && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
