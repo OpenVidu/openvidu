d42d9667f33087eb5f4e09a9e1478e1476950307
pabloFuente <pablofuenteperez@gmail.com>
2020-04-21 19:47:21 +0200

    openvidu-server-params to openvidu-config

--- openvidu-java-client/src/main/java/io/openvidu/java/client/Recording.java
+++ openvidu-java-client/src/main/java/io/openvidu/java/client/Recording.java
@@ -195,7 +195,7 @@ public class Recording {
 	/**
 	 * URL of the recording. You can access the file from there. It is
 	 * <code>null</code> until recording reaches "ready" or "failed" status. If
-	 * <a href="https://docs.openvidu.io/en/stable/reference-docs/openvidu-server-params/" target=
+	 * <a href="https://docs.openvidu.io/en/stable/reference-docs/openvidu-config/" target=
 	 * "_blank">OpenVidu Server configuration</a> property
 	 * <code>OPENVIDU_RECORDING_PUBLIC_ACCESS</code> is false, this path will be
 	 * secured with OpenVidu credentials
