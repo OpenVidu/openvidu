f2f12ac0d14d8ed6dd63bcaa563c997070568ad4
pabloFuente <pablofuenteperez@gmail.com>
2020-04-21 13:37:36 +0200

    discover_my_public_ip.sh updated with new URLs

--- openvidu-server/docker/openvidu-server/discover_my_public_ip.sh
+++ openvidu-server/docker/openvidu-server/discover_my_public_ip.sh
@@ -20,12 +20,13 @@ function valid_ip()
 
 # Services to get public ip
 SERVICES=(
+    "curl --silent -sw :%{http_code} ipv4.icanhazip.com"
     "curl --silent -sw :%{http_code} ifconfig.me"
+    "curl --silent -sw :%{http_code} -4 ifconfig.co"
     "curl --silent -sw :%{http_code} ipecho.net/plain"
     "curl --silent -sw :%{http_code} ipinfo.io/ip"
     "curl --silent -sw :%{http_code} checkip.amazonaws.com"
-    "curl --silent -sw :%{http_code} ident.me"
-    "curl --silent -sw :%{http_code} icanhazip.com"
+    "curl --silent -sw :%{http_code} v4.ident.me"
 )
 
 # Get public ip
