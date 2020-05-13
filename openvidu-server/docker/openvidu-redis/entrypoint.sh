1a0713ebbb23c2835adb66004df47173ead3df82
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-05-13 16:25:53 +0200

    secure redis

---
+++ openvidu-server/docker/openvidu-redis/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+[ -z "${REDIS_BINDING}" ] && REDIS_BINDING="127.0.0.1"
+
+printf "\n"
+printf "\n  ======================================="
+printf "\n  =            REDIS CONF               ="
+printf "\n  ======================================="
+printf "\n"
+
+printf "\n  REDIS_BINDING: %s" "${REDIS_BINDING}"
+printf "\n  REDIS_PASSWORD: %s" "${REDIS_PASSWORD}"
+
+mkdir -p /usr/local/etc/redis
+cat>/usr/local/etc/redis/redis.conf<<EOF
+bind ${REDIS_BINDING}
+requirepass ${REDIS_PASSWORD}
+EOF
+
+printf "\n"
+printf "\n  ======================================="
+printf "\n  =            START REDIS              ="
+printf "\n  ======================================="
+printf "\n"
+
+redis-server /usr/local/etc/redis/redis.conf
