a8467fc868ac675d9db9bf2bb77054c2f02b9917
pabloFuente <pablofuenteperez@gmail.com>
2020-06-01 02:00:23 +0200

    openvidu-server: MediaNodeStatusManager#isCanceled

--- openvidu-server/src/main/java/io/openvidu/server/utils/MediaNodeStatusManager.java
+++ openvidu-server/src/main/java/io/openvidu/server/utils/MediaNodeStatusManager.java
@@ -4,6 +4,8 @@ public interface MediaNodeStatusManager {
 
 	public boolean isLaunching(String mediaNodeId);
 
+	public boolean isCanceled(String mediaNodeId);
+
 	public boolean isRunning(String mediaNodeId);
 
 	public boolean isTerminating(String mediaNodeId);
