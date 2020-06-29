3b5668d828f1ceed25e8ef722e4cdb26b7a78d46
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-06-29 16:37:26 +0200

    Proxy updated for auto renew letscrypt certificated

--- openvidu-server/docker/openvidu-proxy/create_image.sh
+++ openvidu-server/docker/openvidu-proxy/create_image.sh
@@ -2,7 +2,7 @@ VERSION=$1
 if [[ ! -z $VERSION ]]; then
     cp ../utils/discover_my_public_ip.sh ./discover_my_public_ip.sh
 
-    docker build -t openvidu/openvidu-nginx:$VERSION .
+    docker build -t openvidu/openvidu-proxy:$VERSION .
 
     rm ./discover_my_public_ip.sh
 else
