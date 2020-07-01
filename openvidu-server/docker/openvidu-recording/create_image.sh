5477e52db1086b7e482adea795262f71ae49ad62
cruizba <charlesrb94@outlook.com>
2020-07-01 20:21:35 +0200

    Minor fix to create_image recording script

--- openvidu-server/docker/openvidu-recording/create_image.sh
+++ openvidu-server/docker/openvidu-recording/create_image.sh
@@ -2,5 +2,5 @@
 OPENVIDU_RECORDING_UBUNTU_VERSION=$1
 OPENVIDU_RECORDING_CHROME_VERSION=$2
 OPENVIDU_RECORDING_DOCKER_TAG=$3
-docker build --rm --build-arg CHROME_VERSION="$CHROME_VERSION" -f $UBUNTU_VERSION.Dockerfile -t openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG .
+docker build --rm --build-arg CHROME_VERSION="$OPENVIDU_RECORDING_DOCKER_TAG" -f $UBUNTU_VERSION.Dockerfile -t openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG .
 docker push openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG
