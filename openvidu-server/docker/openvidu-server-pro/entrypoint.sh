76805ff54354e2409ca05706f59fecc04c5bef05
cruizba <charlesrb94@outlook.com>
2020-11-02 21:15:18 +0100

    Minor fix entrypoint openvidu-server and openvidu-server-pro docker images

--- openvidu-server/docker/openvidu-server-pro/entrypoint.sh
+++ openvidu-server/docker/openvidu-server-pro/entrypoint.sh
@@ -25,9 +25,9 @@ printf "\n"
 
 # Get coturn public ip
 [[ -z "${COTURN_IP}" ]] && export COTURN_IP=auto-ipv4
-if [[ -z "${COTURN_IP}" == "auto-ipv4" ]]; then
+if [[ "${COTURN_IP}" == "auto-ipv4" ]]; then
     COTURN_IP=$(/usr/local/bin/discover_my_public_ip.sh)
-elif [[ -z "${COTURN_IP}" == "auto-ipv6" ]]; then
+elif [[ "${COTURN_IP}" == "auto-ipv6" ]]; then
     COTURN_IP=$(/usr/local/bin/discover_my_public_ip.sh --ipv6)
 fi
 
