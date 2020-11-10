d1dd6a27cbe4628cfea351d2817cb6e3cdf50f80
pabloFuente <pablofuenteperez@gmail.com>
2020-11-10 19:29:45 +0100

    openvidu-recording image to 2.16.0

--- openvidu-server/docker/openvidu-recording/create_image.sh
+++ openvidu-server/docker/openvidu-recording/create_image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+
+# eg: $ ./create_image.sh ubuntu-20-04 86.0.4240.193-1 2.16.0
 OPENVIDU_RECORDING_UBUNTU_VERSION=$1
 OPENVIDU_RECORDING_CHROME_VERSION=$2 # https://www.ubuntuupdates.org/package_logs?noppa=&page=1&type=ppas&vals=8#
 OPENVIDU_RECORDING_DOCKER_TAG=$3
