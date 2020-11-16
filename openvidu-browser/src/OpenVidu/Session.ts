0a1a5e1e67df998178217e8305951e6f2cf528c9
pabloFuente <pablofuenteperez@gmail.com>
2020-11-16 14:06:18 +0100

    openvidu-browser: sendVideoData also in Edge

--- openvidu-browser/src/OpenVidu/Session.ts
+++ openvidu-browser/src/OpenVidu/Session.ts
@@ -1165,9 +1165,9 @@ export class Session extends EventDispatcher {
     sendVideoData(streamManager: StreamManager, intervalSeconds: number = 1, doInterval: boolean = false, maxLoops: number = 1) {
         if (
             platform.isChromeBrowser() || platform.isChromeMobileBrowser() || platform.isOperaBrowser() ||
-            platform.isOperaMobileBrowser() || platform.isElectron() || (platform.isSafariBrowser() && !platform.isIonicIos()) ||
-            platform.isAndroidBrowser() || platform.isSamsungBrowser() || platform.isIonicAndroid() ||
-            (platform.isIPhoneOrIPad() && platform.isIOSWithSafari())
+            platform.isOperaMobileBrowser() || platform.isEdgeBrowser() || platform.isElectron() ||
+            (platform.isSafariBrowser() && !platform.isIonicIos()) || platform.isAndroidBrowser() ||
+            platform.isSamsungBrowser() || platform.isIonicAndroid() || (platform.isIPhoneOrIPad() && platform.isIOSWithSafari())
         ) {
             const obtainAndSendVideo = async () => {
                 const statsMap = await streamManager.stream.getRTCPeerConnection().getStats();
