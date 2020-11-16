d1dbcf88ab6c861a66902933d85ac65752fa4442
pabloFuente <pablofuenteperez@gmail.com>
2020-11-16 16:43:40 +0100

    openvidu-server dashboard to Angular 11

--- openvidu-server/src/dashboard/src/app/app.routing.ts
+++ openvidu-server/src/dashboard/src/app/app.routing.ts
@@ -41,4 +41,4 @@ const appRoutes: Routes = [
   }
 ];
 
-export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes, { useHash: true });
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes, { useHash: true, relativeLinkResolution: 'legacy' });
--- openvidu-server/src/dashboard/src/app/components/session-details/session-details.component.spec.ts
+++ openvidu-server/src/dashboard/src/app/components/session-details/session-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SessionDetailsComponent } from './session-details.component';
 
@@ -6,7 +6,7 @@ describe('SessionDetailsComponent', () => {
   let component: SessionDetailsComponent;
   let fixture: ComponentFixture<SessionDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SessionDetailsComponent ]
     })
