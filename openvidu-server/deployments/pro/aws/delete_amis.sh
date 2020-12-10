2e0332f4052005c612073361532dc00cedd95e60
cruizba <charlesrb94@outlook.com>
2020-12-10 12:00:23 +0100

    openvidu-deployment: delete_amis.sh: Process AMI_LIST

--- openvidu-server/deployments/pro/aws/delete_amis.sh
+++ openvidu-server/deployments/pro/aws/delete_amis.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -x
 set -eu -o pipefail
 
+# Process AMI_LIST
+AMI_LIST=($(echo "$AMI_LIST" | tr ',' '\n'))
+
 # Remove the list of AMIs in each region
-for line in ${AMI_LIST}
+for line in "${AMI_LIST[@]}"
 do
 	REGION=$(echo "${line}" | cut -d":" -f1)
 	AMI_ID=$(echo "${line}" | cut -d":" -f2)
       export AWS_DEFAULT_REGION=${REGION}
-      
+
       mapfile -t SNAPSHOTS < <(aws ec2 describe-images --image-ids "$AMI_ID" --output text --query 'Images[*].BlockDeviceMappings[*].Ebs.SnapshotId')
       echo "Deregistering $AMI_ID"
 	aws ec2 deregister-image --image-id "${AMI_ID}"
