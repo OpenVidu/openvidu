23eed959371006bb784112ceb14e6c01c4eff0c0
pabloFuente <pablofuenteperez@gmail.com>
2021-08-10 20:03:12 +0200

    openvidu-server: fix typo

--- openvidu-server/src/main/java/io/openvidu/server/core/Participant.java
+++ openvidu-server/src/main/java/io/openvidu/server/core/Participant.java
@@ -314,7 +314,7 @@ public class Participant {
 		json.addProperty("sessionId", this.sessionId);
 		json.addProperty("createdAt", this.token.getCreatedAt());
 		json.addProperty("activeAt", this.activeAt);
-		json.addProperty("location", his.location.toString());
+		json.addProperty("location", this.location.toString());
 		json.addProperty("ip", this.location.getIp());
 		json.addProperty("platform", this.platform);
 		if (this.token.getToken() != null) {
