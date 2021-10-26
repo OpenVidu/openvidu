2e8536064b4fd30a75f12af181dd21f2d759a1e9
pabloFuente <pablofuenteperez@gmail.com>
2021-10-26 14:41:26 +0200

    Jenkinsfile: use checkout plugin instead of git clone command

--- openvidu-test-e2e/jenkins/commonFunctions.groovy
+++ openvidu-test-e2e/jenkins/commonFunctions.groovy
@@ -3,6 +3,15 @@ def prepareTestingEnvironment() {
     println('Deleting folder /opt/openvidu')
     sh 'sudo rm -rf /opt/openvidu/* || true'
 
+    println('Deleting repositories')
+    sh 'sudo rm -rf openvidu-pro || true'
+    sh 'sudo rm -rf openvidu || true'
+    sh 'sudo rm -rf kurento-java || true'
+
+    println('Deleting OpenVidu related .m2 dependencies')
+    sh 'sudo rm -rf /opt/openvidu-cache/.m2/repository/io/openvidu || true'
+    sh 'sudo rm -rf /opt/openvidu-cache/.m2/repository/org/kurento || true'
+
     println('Removing stranded containers')
     sh(script: '''#!/bin/bash -xe
         declare -a arr=('openvidu/openvidu-test-e2e:',
@@ -57,9 +66,9 @@ def prepareTestingEnvironment() {
             sh(script: '''#!/bin/bash -xe
                 FAKE_VIDEO=/opt/openvidu-cache/barcode.y4m
                 if [ ! -f $FAKE_VIDEO ]; then
-                sudo curl --location https://github.com/OpenVidu/openvidu/raw/master/openvidu-test-e2e/docker/barcode.y4m --create-dirs --output /opt/openvidu-cache/barcode.y4m
+                    sudo curl --location https://github.com/OpenVidu/openvidu/raw/master/openvidu-test-e2e/docker/barcode.y4m --create-dirs --output /opt/openvidu-cache/barcode.y4m
                 else
-                echo "File $FAKE_VIDEO already exists"
+                    echo "File $FAKE_VIDEO already exists"
                 fi
                 sudo cp /opt/openvidu-cache/barcode.y4m /opt/openvidu/barcode.y4m
             '''.stripIndent())
@@ -68,9 +77,9 @@ def prepareTestingEnvironment() {
             sh(script: '''#!/bin/bash -xe
                 FAKE_AUDIO=/opt/openvidu-cache/fakeaudio.wav
                 if [ ! -f $FAKE_AUDIO ]; then
-                sudo curl --location https://github.com/OpenVidu/openvidu/raw/master/openvidu-test-e2e/docker/fakeaudio.wav --create-dirs --output /opt/openvidu-cache/fakeaudio.wav
+                    sudo curl --location https://github.com/OpenVidu/openvidu/raw/master/openvidu-test-e2e/docker/fakeaudio.wav --create-dirs --output /opt/openvidu-cache/fakeaudio.wav
                 else
-                echo "File $FAKE_AUDIO already exists"
+                    echo "File $FAKE_AUDIO already exists"
                 fi
                 sudo cp /opt/openvidu-cache/barcode.y4m /opt/openvidu/fakeaudio.wav
             '''.stripIndent())
