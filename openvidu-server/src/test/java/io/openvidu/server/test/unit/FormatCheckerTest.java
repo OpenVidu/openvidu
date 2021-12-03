da2758a93edb5dfc3a2bb6cfd32095c798c39060
pabloFuente <pablofuenteperez@gmail.com>
2021-12-03 11:45:27 +0100

    Test commit trigger, again

--- openvidu-server/src/test/java/io/openvidu/server/test/unit/FormatCheckerTest.java
+++ openvidu-server/src/test/java/io/openvidu/server/test/unit/FormatCheckerTest.java
@@ -20,9 +20,9 @@ public class FormatCheckerTest {
 				"&session", "ses=sion", "+", "session,", "/session", "session?", "session#", "session%", "[session]",
 				"session.", "session~", "~session", "session~1", "\\session");
 
-		List<String> validCustomSessionIds = Arrays.asList("s", "1", "-", "_", "session", "session1", "0session10",
-				"-session", "session-", "-session-", "_session", "session_", "_session_", "_-session", "session-_",
-				"123_session-1");
+		List<String> validCustomSessionIds = Arrays.asList("s", "1", "-", "_", "_-_", "session", "session1",
+				"0session10", "-session", "session-", "-session-", "_session", "session_", "_session_", "_-session",
+				"session-_", "123_session-1");
 
 		FormatChecker formatChecker = new FormatChecker();
 		for (String id : invalidCustomSessionIds)
