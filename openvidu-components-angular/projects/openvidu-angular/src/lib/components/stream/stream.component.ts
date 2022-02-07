51c63fa4c5fcb1359b01a039fc5527aa7288bc3b
csantosm <4a.santos@gmail.com>
2022-02-07 10:33:49 +0100

    openvidu-components: Added stream notification projection

--- openvidu-components-angular/projects/openvidu-angular/src/lib/components/stream/stream.component.ts
+++ openvidu-components-angular/projects/openvidu-angular/src/lib/components/stream/stream.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ContentChild, ElementRef, HostListener, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatMenuPanel, MatMenuTrigger } from '@angular/material/menu';
 import { NicknameMatcher } from '../../matchers/nickname.matcher';
@@ -22,6 +22,8 @@ import { ParticipantService } from '../../services/participant/participant.servi
 	styleUrls: ['./stream.component.css']
 })
 export class StreamComponent implements OnInit {
+	@ContentChild('notification', { read: TemplateRef }) notificationTemplate: TemplateRef<any>;
+
 	videoSizeIconEnum = VideoSizeIcon;
 	videoTypeEnum = VideoType;
 	videoSizeIcon: VideoSizeIcon = VideoSizeIcon.BIG;
