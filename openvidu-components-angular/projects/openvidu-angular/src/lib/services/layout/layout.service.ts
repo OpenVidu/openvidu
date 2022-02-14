ead068fbeec454dcf69a4105668c1b3dec110b3e
csantosm <4a.santos@gmail.com>
2022-02-14 10:41:55 +0100

    openvidu-components: Improved video aspect ratios in layout

--- openvidu-components-angular/projects/openvidu-angular/src/lib/services/layout/layout.service.ts
+++ openvidu-components-angular/projects/openvidu-angular/src/lib/services/layout/layout.service.ts
@@ -36,14 +36,14 @@ export class LayoutService {
 	getOptions(): OpenViduLayoutOptions {
 		const options = {
 			maxRatio: 3 / 2, // The narrowest ratio that will be used (default 2x3)
-			minRatio: 9 / 15, // The widest ratio that will be used (default 16x9)
+			minRatio: 9 / 16, // The widest ratio that will be used (default 16x9)
 			fixedRatio: false /* If this is true then the aspect ratio of the video is maintained
       and minRatio and maxRatio are ignored (default false) */,
 			bigClass: LayoutClass.BIG_ELEMENT, // The class to add to elements that should be sized bigger
 			smallClass: LayoutClass.SMALL_ELEMENT,
-			bigPercentage: 0.85, // The maximum percentage of space the big ones should take up
+			bigPercentage: 0.7, // The maximum percentage of space the big ones should take up
 			bigFixedRatio: false, // fixedRatio for the big ones
-			bigMaxRatio: 3 / 2, // The narrowest ratio to use for the big elements (default 2x3)
+			bigMaxRatio: 9 / 16, // The narrowest ratio to use for the big elements (default 2x3)
 			bigMinRatio: 9 / 16, // The widest ratio to use for the big elements (default 16x9)
 			bigFirst: true, // Whether to place the big one in the top left (true) or bottom right
 			animate: true // Whether you want to animate the transitions. Invalid property, to disable it remove   transition: all .1s linear;
