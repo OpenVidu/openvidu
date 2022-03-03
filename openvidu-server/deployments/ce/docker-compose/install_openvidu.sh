08b7c198308a53981314d865734570ac25b4dde1
pabloFuente <pablofuenteperez@gmail.com>
2022-03-03 14:31:57 +0100

    Update OPENVIDU_UPGRADABLE_VERSION to 2.20 in all installation scripts

--- openvidu-server/deployments/ce/docker-compose/install_openvidu.sh
+++ openvidu-server/deployments/ce/docker-compose/install_openvidu.sh
@@ -3,7 +3,7 @@
 # Global variables
 OPENVIDU_FOLDER=openvidu
 OPENVIDU_VERSION=master
-OPENVIDU_UPGRADABLE_VERSION="2.18"
+OPENVIDU_UPGRADABLE_VERSION="2.20"
 DOWNLOAD_URL=https://raw.githubusercontent.com/OpenVidu/openvidu/${OPENVIDU_VERSION}
 
 fatal_error() {
