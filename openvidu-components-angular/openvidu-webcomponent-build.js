1ec723edd41a8b99b49f3a07461198f6fb9ae86d
csantosm <4a.santos@gmail.com>
2022-04-07 09:50:07 +0200

    openvidu-components: Updated webcomponent build script

--- openvidu-components-angular/openvidu-webcomponent-build.js
+++ openvidu-components-angular/openvidu-webcomponent-build.js
@@ -1,11 +1,14 @@
-const fs = require("fs-extra");
-const concat = require("concat");
-const VERSION = require("./package.json").version;
+const fs = require('fs-extra');
+const concat = require('concat');
+const VERSION = require('./package.json').version;
+const ovWebcomponentRCPath = './dist/openvidu-webcomponent-rc';
+const ovWebcomponentProdPath = './dist/openvidu-webcomponent';
 
 module.exports.buildWebcomponent = async () => {
-	console.log("Building OpenVidu Web Component (" + VERSION + ")");
-	const tutorialWcPath = "../../openvidu-tutorials/openvidu-webcomponent/web";
-	const e2eWcPath = "./e2e/webcomponent-app";
+	console.log('Building OpenVidu Web Component (' + VERSION + ')');
+	const tutorialWcPath = '../../openvidu-tutorials/openvidu-webcomponent/web';
+	const e2eWcPath = './e2e/webcomponent-app';
+
 
 	try {
 		await buildElement();
@@ -13,35 +16,25 @@ module.exports.buildWebcomponent = async () => {
 		await copyFiles(e2eWcPath);
 		await renameWebComponentTestName(e2eWcPath);
 
-		console.log("OpenVidu Web Component (" + VERSION + ") built");
+		console.log(`OpenVidu Web Component (${VERSION}) built`);
 	} catch (error) {
 		console.error(error);
 	}
 };
 
 async function buildElement() {
-	const files = [
-		"./dist/openvidu-webcomponent/runtime.js",
-		"./dist/openvidu-webcomponent/main.js",
-		"./dist/openvidu-webcomponent/polyfills.js"
-	];
+	const files = [`${ovWebcomponentRCPath}/runtime.js`, `${ovWebcomponentRCPath}/main.js`, `${ovWebcomponentRCPath}/polyfills.js`];
 
 	try {
-		await fs.ensureDir("openvidu-webcomponent");
-		await concat(
-			files,
-			"./openvidu-webcomponent/openvidu-webcomponent-" + VERSION + ".js"
-		);
-		await fs.copy(
-			"./dist/openvidu-webcomponent/styles.css",
-			"./openvidu-webcomponent/openvidu-webcomponent-" + VERSION + ".css"
-		);
-		await fs.copy(
-			"./dist/openvidu-webcomponent/assets",
-			"./openvidu-webcomponent/assets"
-		);
+		await fs.ensureDir('./dist/openvidu-webcomponent');
+		await concat(files, `${ovWebcomponentProdPath}/openvidu-webcomponent-${VERSION}.js`);
+		await fs.copy(`${ovWebcomponentRCPath}/styles.css`, `${ovWebcomponentProdPath}/openvidu-webcomponent-${VERSION}.css`);
+		// await fs.copy(
+		// 	"./dist/openvidu-webcomponent/assets",
+		// 	"./openvidu-webcomponent/assets"
+		// );
 	} catch (err) {
-		console.error("Error executing build function in webcomponent-builds.js");
+		console.error('Error executing build function in webcomponent-builds.js');
 		throw err;
 	}
 }
@@ -54,14 +47,11 @@ function renameWebComponentTestName(dir) {
 async function copyFiles(destination) {
 	if (fs.existsSync(destination)) {
 		try {
-			console.log(
-				"Copying openvidu-webcomponent files from: ./openvidu-webcomponent to: " +
-					destination
-			);
-			await fs.ensureDir("openvidu-webcomponent");
-			await fs.copy("./openvidu-webcomponent/", destination);
+			console.log(`Copying openvidu-webcomponent files from: ${ovWebcomponentProdPath} to: ${destination}`);
+			await fs.ensureDir(ovWebcomponentProdPath);
+			await fs.copy(ovWebcomponentProdPath, destination);
 		} catch (err) {
-			console.error("Error executing copy function in webcomponent-builds.js");
+			console.error('Error executing copy function in webcomponent-builds.js');
 			throw err;
 		}
 	}
