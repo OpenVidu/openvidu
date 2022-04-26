1655335aa7132009767eb1f35c8e8b055e2bb26e
cruizba <carlos.ruizbal@gmail.com>
2022-04-26 18:27:57 +0200

    Update generate docs and package.json

--- openvidu-components-angular/generate-docs.sh
+++ openvidu-components-angular/generate-docs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# if [[ -z "$BASEHREF_VERSION" ]]; then
-#     echo "Example of use: \"BASEHREF_VERSION=2.12.0 ${0}\"" 1>&2
-#     exit 1
-# fi
+if [[ -z "$BASEHREF_VERSION" ]]; then
+    echo "Example of use: \"BASEHREF_VERSION=2.12.0 ${0}\"" 1>&2
+    exit 1
+fi
 
 # Replace version from "stable" to the specified one in all TypeDoc links
-# grep -rl '/en/stable/' src | xargs sed -i -e 's|/en/stable/|/en/'${BASEHREF_VERSION}'/|g'
+grep -rl '/en/stable/' projects src | xargs sed -i -e 's|/en/stable/|/en/'${BASEHREF_VERSION}'/|g'
 
 # Generate Compodoc
-npm run lib:doc-build
+npm run doc:build
 
 # Return links to "stable" version
-# grep -rl '/en/'${BASEHREF_VERSION}'/' src | xargs sed -i -e 's|/en/'${BASEHREF_VERSION}'/|/en/stable/|g'
+grep -rl '/en/'${BASEHREF_VERSION}'/' projects src | xargs sed -i -e 's|/en/'${BASEHREF_VERSION}'/|/en/stable/|g'
 
 # Clean previous docs from openvidu.io-docs repo and copy new ones
-npm run lib:clean-copy
+npm run doc:clean-copy
 
