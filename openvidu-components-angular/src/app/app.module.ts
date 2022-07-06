873b52f19c05a0da828850b74578a8d5b814ca45
csantosm <4a.santos@gmail.com>
2022-07-06 13:55:15 +0200

    openvidu-components: Updated testapp

--- openvidu-components-angular/src/app/app.module.ts
+++ openvidu-components-angular/src/app/app.module.ts
@@ -14,42 +14,12 @@ import { environment } from 'src/environments/environment';
 import { CallComponent } from './openvidu-call/call.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TestingComponent } from './testing-app/testing.component';
-import { ToolbarDirectiveComponent } from './examples/toolbar-directive/toolbar-directive.component';
-import { ToolbarAdditionalButtonsDirectiveComponent } from './examples/toolbarAdditionalButtons-directive/toolbarAdditionalButtons-directive.component';
-import { ToolbarAdditionalPanelButtonsDirectiveComponent } from './examples/toolbarAdditionalPanelButtons-directive/toolbarAdditionalPanelButtons-directive.component';
-import { LayoutDirectiveComponent } from './examples/layout-directive/layout-directive.component';
-import { StreamDirectiveComponent } from './examples/stream-directive/stream-directive.component';
-import { PanelDirectiveComponent } from './examples/panel-directive/panel-directive.component';
-import { AdditionalPanelsDirectiveComponent } from './examples/additionalPanels-directive/additionalPanels-directive.component';
-import { ParticipantsPanelDirectiveComponent } from './examples/participantsPanel-directive/participantsPanel-directive.component';
-import { ParticipantPanelItemDirectiveComponent } from './examples/participantPanelItem-directive/participantPanelItem-directive.component';
-import { ParticipantPanelItemElementsDirectiveComponent } from './examples/participantPanelItemElements-directive/participantPanelItemElements-directive.component';
-import { ChatPanelDirectiveComponent } from './examples/chatPanel-directive/chatPanel-directive.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 // openvidu-angular
 import { OpenViduAngularModule } from 'openvidu-angular';
-import { ExamplesDashboardComponent } from './examples/examples-dashboard/examples-dashboard.component';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		DashboardComponent,
-		AdminDashboardComponent,
-		CallComponent,
-		TestingComponent,
-		ExamplesDashboardComponent,
-		ToolbarDirectiveComponent,
-		ToolbarAdditionalButtonsDirectiveComponent,
-		ToolbarAdditionalPanelButtonsDirectiveComponent,
-		LayoutDirectiveComponent,
-		StreamDirectiveComponent,
-		PanelDirectiveComponent,
-		AdditionalPanelsDirectiveComponent,
-		ParticipantsPanelDirectiveComponent,
-		ChatPanelDirectiveComponent,
-		ParticipantPanelItemDirectiveComponent,
-		ParticipantPanelItemElementsDirectiveComponent
-	],
+	declarations: [AppComponent, DashboardComponent, AdminDashboardComponent, CallComponent, TestingComponent],
 	imports: [
 		BrowserModule,
 		MatCheckboxModule,
