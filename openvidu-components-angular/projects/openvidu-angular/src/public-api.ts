1d70791fd9f30008b7dc2eec97ccb39afa99777e
csantosm <4a.santos@gmail.com>
2022-07-14 09:39:11 +0200

    openvidu-components: Exported only the necessary elements

--- openvidu-components-angular/projects/openvidu-angular/src/public-api.ts
+++ openvidu-components-angular/projects/openvidu-angular/src/public-api.ts
@@ -8,17 +8,9 @@ export * from './lib/openvidu-angular.module';
 export * from './lib/services/openvidu/openvidu.service';
 export * from './lib/services/participant/participant.service';
 export * from './lib/services/chat/chat.service';
-export * from './lib/services/platform/platform.service';
-export * from './lib/services/logger/logger.service';
-export * from './lib/services/config/openvidu-angular.config.service';
-export * from './lib/services/document/document.service';
-export * from './lib/services/token/token.service';
-export * from './lib/services/device/device.service';
 export * from './lib/services/action/action.service';
 export * from './lib/services/layout/layout.service';
 export * from './lib/services/panel/panel.service';
-export * from './lib/services/cdk-overlay/cdk-overlay.service';
-export * from './lib/services/storage/storage.service';
 export * from './lib/services/recording/recording.service';
 
 // Components
@@ -26,26 +18,17 @@ export * from './lib/components/videoconference/videoconference.component';
 export * from './lib/components/toolbar/toolbar.component';
 export * from './lib/components/panel/panel.component';
 export * from './lib/components/panel/chat-panel/chat-panel.component';
-export * from './lib/components/panel/settings-panel/settings-panel.component';
-export * from './lib/components/panel/background-effects-panel/background-effects-panel.component';
-export * from './lib/components/panel/activities-panel/activities-panel.component';
 export * from './lib/components/panel/participants-panel/participants-panel/participants-panel.component';
 export * from './lib/components/panel/participants-panel/participant-panel-item/participant-panel-item.component';
-export * from './lib/components/session/session.component';
 export * from './lib/components/layout/layout.component';
 export * from './lib/components/stream/stream.component';
-export * from './lib/components/video/video.component';
-export * from './lib/components/audio-wave/audio-wave.component';
-export * from './lib/components/pre-join/pre-join.component';
 export * from './lib/admin/dashboard/dashboard.component';
 export * from './lib/admin/login/login.component';
 
 // Models
 export * from './lib/models/participant.model';
 export * from './lib/config/openvidu-angular.config';
-export * from './lib/models/logger.model';
 export * from './lib/models/video-type.model';
-export * from './lib/models/notification-options.model';
 export * from './lib/models/token.model';
 export * from './lib/models/signal.model';
 export * from './lib/models/panel.model';
