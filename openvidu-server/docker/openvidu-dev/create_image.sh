d878c3f5ebd3a952e2b1f2263f93df309ac4ca2d
pabloFuente <pablofuenteperez@gmail.com>
2022-08-08 23:49:22 +0200

    Rename docker container openvidu-server-kms to openvidu-dev

--- openvidu-server/docker/openvidu-dev/create_image.sh
+++ openvidu-server/docker/openvidu-dev/create_image.sh
@@ -1,7 +1,7 @@
 VERSION=$1
 if [[ ! -z $VERSION ]]; then
     cp ../../target/openvidu-server-*.jar ./openvidu-server.jar
-    docker build --pull --no-cache --rm=true -t openvidu/openvidu-server-kms:$VERSION .
+    docker build --pull --no-cache --rm=true -t openvidu/openvidu-dev:$VERSION .
     rm ./openvidu-server.jar
 else
     echo "Error: You need to specify a version as first argument"
