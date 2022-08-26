a2c6c8b4eb64d8eabc0af808327f6ba3a173e8d2
pabloFuente <pablofuenteperez@gmail.com>
2022-08-26 11:45:25 +0200

    Rename openvidu-js-java(/node) to openvidu-roles-java(/node)

--- update-plainjs-prod.sh
+++ update-plainjs-prod.sh
@@ -9,17 +9,11 @@ cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials
 # openvidu-filters
 cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-filters/web/openvidu-browser-"$1".min.js
 
-# openvidu-js-java
-cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-js-java/src/main/resources/static/openvidu-browser-"$1".min.js
+# openvidu-roles-java
+cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-roles-java/src/main/resources/static/openvidu-browser-"$1".min.js
 
-# openvidu-mvc-java
-cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-mvc-java/src/main/resources/static/openvidu-browser-"$1".min.js
-
-# openvidu-js-node
-cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-js-node/public/openvidu-browser-"$1".min.js
-
-# openvidu-mvc-node
-cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-mvc-node/public/openvidu-browser-"$1".min.js
+# openvidu-roles-node
+cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-roles-node/public/openvidu-browser-"$1".min.js
 
 # openvidu-getaroom
 cp openvidu-browser/static/js/openvidu-browser-"$1".min.js ../openvidu-tutorials/openvidu-getaroom/web/openvidu-browser-"$1".min.js
