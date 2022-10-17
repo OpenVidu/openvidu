d4cbc678364562688bcc4f4d526ad1f222f217bb
Carlos Santos <4a.santos@gmail.com>
2022-10-17 11:42:04 +0200

    openvidu-components: Replaced selenium image by browserless
    Significantly reduced the e2e tests time using browserless chrome instead of selenium standalone

--- openvidu-components-angular/e2e/selenium.conf.ts
+++ openvidu-components-angular/e2e/selenium.conf.ts
@@ -1,6 +1,6 @@
-import { LAUNCH_MODE } from './config';
-import * as chrome from 'selenium-webdriver/chrome';
 import { Capabilities } from 'selenium-webdriver';
+import * as chrome from 'selenium-webdriver/chrome';
+import { LAUNCH_MODE } from './config';
 
 interface BrowserConfig {
 	appUrl: string;
@@ -13,15 +13,17 @@ interface BrowserConfig {
 let chromeArguments = ['--window-size=1024,768', '--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'];
 let chromeArgumentsCI = [
 	'--headless',
+	'--no-sandbox',
+	'--disable-extensions',
+	'--disable-gpu',
 	'--disable-dev-shm-usage',
-	'--window-size=1024,768',
 	'--use-fake-ui-for-media-stream',
 	'--use-fake-device-for-media-stream'
 ];
 
 export const WebComponentConfig: BrowserConfig = {
 	appUrl: 'http://localhost:8080/',
-	seleniumAddress: LAUNCH_MODE === 'CI' ? 'http://localhost:4444/wd/hub' : '',
+	seleniumAddress: LAUNCH_MODE === 'CI' ? 'http://localhost:3000/webdriver' : '',
 	browserName: 'chrome',
 	browserCapabilities: Capabilities.chrome().set('acceptInsecureCerts', true),
 	browserOptions: new chrome.Options().addArguments(...(LAUNCH_MODE === 'CI' ? chromeArgumentsCI : chromeArguments))
@@ -29,7 +31,7 @@ export const WebComponentConfig: BrowserConfig = {
 
 export const AngularConfig: BrowserConfig = {
 	appUrl: 'http://localhost:4200/#/testing',
-	seleniumAddress: LAUNCH_MODE === 'CI' ? 'http://localhost:4444/wd/hub' : '',
+	seleniumAddress: LAUNCH_MODE === 'CI' ? 'http://localhost:3000/webdriver' : '',
 	browserName: 'Chrome',
 	browserCapabilities: Capabilities.chrome().set('acceptInsecureCerts', true),
 	browserOptions: new chrome.Options().addArguments(...(LAUNCH_MODE === 'CI' ? chromeArgumentsCI : chromeArguments))
