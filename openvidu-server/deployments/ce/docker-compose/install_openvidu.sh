d71cbaeb880392a6cc79c5e86539eb42d4d59625
pabloFuente <pablofuenteperez@gmail.com>
2022-11-17 19:13:08 +0100

    Update installation scripts to 2.24.0

--- openvidu-server/deployments/ce/docker-compose/install_openvidu.sh
+++ openvidu-server/deployments/ce/docker-compose/install_openvidu.sh
@@ -2,7 +2,7 @@
 
 # Global variables
 OPENVIDU_FOLDER=openvidu
-OPENVIDU_VERSION=master
+OPENVIDU_VERSION=v2.24.0
 OPENVIDU_UPGRADABLE_VERSION="2.23"
 DOWNLOAD_URL=https://raw.githubusercontent.com/OpenVidu/openvidu/${OPENVIDU_VERSION}
 
