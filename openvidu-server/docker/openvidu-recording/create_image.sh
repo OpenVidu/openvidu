b495e94c9ee4607a9ef444e96132d13452a99a08
pabloFuente <pablofuenteperez@gmail.com>
2023-03-07 20:26:18 +0100

    openvidu-recording: udpate create_image.sh script

--- openvidu-server/docker/openvidu-recording/create_image.sh
+++ openvidu-server/docker/openvidu-recording/create_image.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -x
 
-# eg: $ ./create_image.sh 109.0.5414.119-1 2.25.0
-OPENVIDU_RECORDING_CHROME_VERSION=$1 # https://www.ubuntuupdates.org/package_logs?noppa=&page=1&type=ppas&vals=8#
-OPENVIDU_RECORDING_DOCKER_TAG=$2
-docker build --rm --pull --no-cache \
-    --build-arg CHROME_VERSION="$OPENVIDU_RECORDING_CHROME_VERSION" \
-    -t openvidu/openvidu-recording:$OPENVIDU_RECORDING_DOCKER_TAG .
+CHROME_VERSION=$1 # https://www.ubuntuupdates.org/package_logs?noppa=&page=1&type=ppas&vals=8#
+TAG=$2
+
+if [[ ! -z $CHROME_VERSION && ! -z $TAG ]]; then
+    docker build --rm --pull --no-cache --build-arg CHROME_VERSION="$CHROME_VERSION" -t openvidu/openvidu-recording:$TAG .
+else
+    echo "Error: You need to specify a Chrome version as first argument and a docker tag as second argument"
+    echo "./create_image.sh 110.0.5481.177-1 2.26.0"
+    echo "You can obtain a valid Chrome version from https://www.ubuntuupdates.org/package_logs?noppa=&page=1&type=ppas&vals=8#"
+fi
