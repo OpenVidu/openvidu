ff189941bd911788196a42d84c141554e9512897
pabloFuente <pablofuenteperez@gmail.com>
2023-03-15 22:49:54 +0100

    Docker containers: fix create_image.scripts to allow if with wildcards

--- openvidu-server/docker/openvidu-dev/create_image.sh
+++ openvidu-server/docker/openvidu-dev/create_image.sh
@@ -2,16 +2,16 @@
 VERSION=$1
 if [[ ! -z $VERSION ]]; then
 
-    if [[ ! -f "./openvidu-server-*.jar" ]]; then
+    if ! ls ./openvidu-server-*.jar 1>/dev/null 2>&1; then
         cp ../../target/openvidu-server-*.jar .
-    fi
-    if [[ ! -f "./openvidu-server-*.jar" ]]; then
-        echo "Error: openvidu-server JAR not found"
-        exit 1
+        if ! ls ./openvidu-server-*.jar 1>/dev/null 2>&1; then
+            echo "Error: openvidu-server JAR not found"
+            exit 1
+        fi
     fi
 
     docker build --pull --no-cache --rm=true -t openvidu/openvidu-dev:$VERSION .
-    rm ./openvidu-server.jar
+    rm ./openvidu-server-*.jar
 else
     echo "Error: You need to specify a version as first argument"
 fi
--- openvidu-server/docker/openvidu-server/create_image.sh
+++ openvidu-server/docker/openvidu-server/create_image.sh
@@ -2,12 +2,12 @@
 VERSION=$1
 if [[ ! -z $VERSION ]]; then
 
-    if [[ ! -f "./openvidu-server-*.jar" ]]; then
+    if ! ls ./openvidu-server-*.jar 1>/dev/null 2>&1; then
         cp ../../target/openvidu-server-*.jar .
-    fi
-    if [[ ! -f "./openvidu-server-*.jar" ]]; then
-        echo "Error: openvidu-server JAR not found"
-        exit 1
+        if ! ls ./openvidu-server-*.jar 1>/dev/null 2>&1; then
+            echo "Error: openvidu-server JAR not found"
+            exit 1
+        fi
     fi
 
     cp ../utils/discover_my_public_ip.sh ./discover_my_public_ip.sh
@@ -15,7 +15,7 @@ if [[ ! -z $VERSION ]]; then
 
     docker build --pull --no-cache --rm=true -t openvidu/openvidu-server:$VERSION .
 
-    rm ./openvidu-server.jar
+    rm ./openvidu-server-*.jar
     rm ./discover_my_public_ip.sh
     rm ./coturn-shared-key.template
 else
