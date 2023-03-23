90801597364c0dfbf1cb5e695c4f55905bf9b8e0
cruizba <carlos.ruizbal@gmail.com>
2023-03-23 14:57:09 +0100

    openvidu: Add version tag to create_image.sh of openvidu-deployment-tester

--- openvidu-server/docker/openvidu-deployment-tester/create_image.sh
+++ openvidu-server/docker/openvidu-deployment-tester/create_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 VERSION=$1
 if [[ ! -z $VERSION ]]; then
-    docker build --pull --no-cache --rm=true -t openvidu/openvidu-deployment-tester:latest .
+    docker build --pull --no-cache --rm=true -t "openvidu/openvidu-deployment-tester:${VERSION}" .
 else
     echo "Error: You need to specify a version as first argument"
 fi
