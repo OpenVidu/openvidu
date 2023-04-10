de9f195fd5e863758473aa958d9775a1bdd53572
cruizba <carlos.ruizbal@gmail.com>
2023-04-10 11:25:29 +0200

    openvidu-recording: Remove CHROME_VERSION argument

--- openvidu-server/docker/openvidu-recording/create_image.sh
+++ openvidu-server/docker/openvidu-recording/create_image.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -x
 
-CHROME_VERSION=$1 # https://www.ubuntuupdates.org/package_logs?noppa=&page=1&type=ppas&vals=8#
-TAG=$2
+TAG=$1
 
-if [[ ! -z $CHROME_VERSION && ! -z $TAG ]]; then
-    docker build --rm --pull --no-cache --build-arg CHROME_VERSION="$CHROME_VERSION" -t openvidu/openvidu-recording:$TAG .
+if [[ -n $TAG ]]; then
+    docker build --rm --pull --no-cache -t "openvidu/openvidu-recording:$TAG" .
 else
-    echo "Error: You need to specify a Chrome version as first argument and a docker tag as second argument"
-    echo "./create_image.sh 110.0.5481.177-1 2.26.0"
-    echo "You can obtain a valid Chrome version from https://www.ubuntuupdates.org/package_logs?noppa=&page=1&type=ppas&vals=8#"
+    echo "Error: You need to specify a docker tag as second argument"
+    echo "./create_image.sh 2.26.0"
 fi
