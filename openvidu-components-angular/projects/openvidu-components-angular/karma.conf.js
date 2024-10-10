5a40ce90564154493dbd657c5ede3d154f3db832
Carlos Santos <4a.santos@gmail.com>
2024-10-10 20:39:16 +0200

    ov-components: Set headless chrome for unit tests

--- openvidu-components-angular/projects/openvidu-components-angular/karma.conf.js
+++ openvidu-components-angular/projects/openvidu-components-angular/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
