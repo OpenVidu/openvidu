8037253ee05a482e8486634dda389c1118eeb1cb
cruizba <carlos.ruizbal@gmail.com>
2025-01-10 21:58:10 +0100

    docker: rollback to ubuntu 22.04 to openvidu-deployment-tester image

--- openvidu-server/docker/openvidu-deployment-tester/create_image.sh
+++ openvidu-server/docker/openvidu-deployment-tester/create_image.sh
@@ -2,7 +2,7 @@
 
 VERSION=$1
 if [[ ! -z $VERSION ]]; then
-    docker build --pull --no-cache --rm=true -t "openvidu/openvidu-deployment-tester:${VERSION}" .
+    docker build -t "openvidu/openvidu-deployment-tester:${VERSION}" .
 else
     echo "Error: You need to specify a version as first argument"
 fi
