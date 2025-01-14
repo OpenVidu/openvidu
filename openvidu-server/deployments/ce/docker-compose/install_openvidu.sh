448743ba8dce271837d2c110024b3f17de204024
jenkinsopenvidu <openvidu@gmail.com>
2025-01-14 18:22:40 +0000

    Update installation scripts to 2.31.0

--- openvidu-server/deployments/ce/docker-compose/install_openvidu.sh
+++ openvidu-server/deployments/ce/docker-compose/install_openvidu.sh
@@ -2,7 +2,7 @@
 
 # Global variables
 OPENVIDU_FOLDER=openvidu
-OPENVIDU_VERSION=v2
+OPENVIDU_VERSION=v2.31.0
 OPENVIDU_UPGRADABLE_VERSION="2.30"
 DOWNLOAD_URL=https://raw.githubusercontent.com/OpenVidu/openvidu/${OPENVIDU_VERSION}
 
