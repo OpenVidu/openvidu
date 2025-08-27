d68cb4933e5594002f0ca8ce2cc70795e6d2d276
Piwccle <sergiosergi11@hotmail.com>
2025-08-27 17:38:50 +0200

    Add Terraform configuration for OpenVidu deployment on GCP
    Changed structure to be more consistant with the terraform standard and removed some resources to try
    
    Refactor terraform main file to be more alike with aws and azure scripts and fixed some things that were wrong in the install script. Changed variables.tf and output.tf as needed
    
    Refactor firewall rules and streamline user data scripts for OpenVidu deployment on GCP
    
    added Elastic deployment for GCP and changed default values of instance type in Single Node and Single Node PRO
    
    openvidu-deployment_ gcp - changed output.tf in all deployments to output the link to secret manager; changed the name of the instance resource of openvidu single node pro; fixed some things that were broken in elastic terraform file and adjusted times for the lambda and the cronjob

--- openvidu-deployment/community/singlenode/gcp/output.tf
+++ openvidu-deployment/community/singlenode/gcp/output.tf
@@ -1,13 +1,5 @@
 # ------------------------- outputs.tf -------------------------
 
-output "openvidu_instance_name" {
-  value = google_compute_instance.openvidu_server.name
-}
-
-output "openvidu_public_ip" {
-  value = length(google_compute_address.public_ip_address) > 0 ? google_compute_address.public_ip_address[0].address : google_compute_instance.openvidu_server.network_interface[0].access_config[0].nat_ip
-}
-
-output "appdata_bucket" {
-  value = local.isEmpty ? google_storage_bucket.bucket[0].name : var.bucketName
+output "secrets_manager" {
+  value = "https://console.cloud.google.com/security/secret-manager?project=${var.projectId}"
 }
