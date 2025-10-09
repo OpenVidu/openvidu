45d2f7dd6eaf47684ed9b8072846f1aeeb581844
Carlos Santos <4a.santos@gmail.com>
2025-10-09 14:27:33 +0200

    ov-components: Updated testapp to Angular 20

--- openvidu-components-angular/projects/openvidu-components-angular/src/lib/services/config/global-config.service.ts
+++ openvidu-components-angular/projects/openvidu-components-angular/src/lib/services/config/global-config.service.ts
@@ -1,5 +1,5 @@
-import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable } from '@angular/core';
+
+import { Inject, Injectable, DOCUMENT } from '@angular/core';
 import { ParticipantFactoryFunction, OpenViduComponentsConfig } from '../../config/openvidu-components-angular.config';
 
 /**
