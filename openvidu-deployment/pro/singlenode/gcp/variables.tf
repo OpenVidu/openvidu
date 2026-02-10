d74c2d03ea4c39a77ed18175dccb07b7771a2e28
Piwccle <sergiosergi11@hotmail.com>
2026-02-10 17:36:47 +0100

    openvidu-deployment: Update default instance type to e2-standard-4 for improved performance

--- openvidu-deployment/pro/singlenode/gcp/variables.tf
+++ openvidu-deployment/pro/singlenode/gcp/variables.tf
@@ -88,7 +88,7 @@ variable "initialMeetApiKey" {
 variable "instanceType" {
   description = "Specifies the GCE machine type for your OpenVidu instance"
   type        = string
-  default     = "e2-standard-2"
+  default     = "e2-standard-4"
 }
 
 variable "bucketName" {
