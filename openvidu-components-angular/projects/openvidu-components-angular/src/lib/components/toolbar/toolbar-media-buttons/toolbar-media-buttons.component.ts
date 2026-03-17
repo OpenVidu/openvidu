2d2da5a659da8ac81f59588fc4938802879b0d54
CSantosM <4a.santos@gmail.com>
2026-03-17 16:26:55 +0100

    ov-components: replace @Input decorators with input() for toolbar media buttons

--- openvidu-components-angular/projects/openvidu-components-angular/src/lib/components/toolbar/toolbar-media-buttons/toolbar-media-buttons.component.ts
+++ openvidu-components-angular/projects/openvidu-components-angular/src/lib/components/toolbar/toolbar-media-buttons/toolbar-media-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, EventEmitter, Input, Output, TemplateRef, computed, inject } from '@angular/core';
+import { Component, ContentChild, EventEmitter, Output, TemplateRef, computed, inject, input } from '@angular/core';
 import { RecordingStatus } from '../../../models/recording.model';
 import { BroadcastingStatus } from '../../../models/broadcasting.model';
 import { ToolbarAdditionalButtonsPosition } from '../../../models/toolbar.model';
@@ -16,62 +16,62 @@ import { ToolbarMoreOptionsAdditionalMenuItemsDirective } from '../../../directi
 })
 export class ToolbarMediaButtonsComponent {
 	// Camera related inputs
-	@Input() showCameraButton: boolean = true;
-	@Input() isCameraEnabled: boolean = true;
-	@Input() cameraMuteChanging: boolean = false;
+	showCameraButton = input<boolean>(true);
+	isCameraEnabled = input<boolean>(true);
+	cameraMuteChanging = input<boolean>(false);
 
 	// Microphone related inputs
-	@Input() showMicrophoneButton: boolean = true;
-	@Input() isMicrophoneEnabled: boolean = true;
-	@Input() microphoneMuteChanging: boolean = false;
+	showMicrophoneButton = input<boolean>(true);
+	isMicrophoneEnabled = input<boolean>(true);
+	microphoneMuteChanging = input<boolean>(false);
 
 	// Screenshare related inputs
-	@Input() showScreenshareButton: boolean = true;
-	@Input() isScreenShareEnabled: boolean = false;
-	@Input() isFirefoxBrowser: boolean = false;
+	showScreenshareButton = input<boolean>(true);
+	isScreenShareEnabled = input<boolean>(false);
+	isFirefoxBrowser = input<boolean>(false);
 
 	// Device availability inputs
-	@Input() hasVideoDevices: boolean = true;
-	@Input() hasAudioDevices: boolean = true;
+	hasVideoDevices = input<boolean>(true);
+	hasAudioDevices = input<boolean>(true);
 
 	// Connection state inputs
-	@Input() isConnectionLost: boolean = false;
+	isConnectionLost = input<boolean>(false);
 
 	// UI state inputs
-	@Input() isMinimal: boolean = false;
+	isMinimal = input<boolean>(false);
 
 	// More options menu inputs
-	@Input() showMoreOptionsButton: boolean = true;
-	@Input() showFullscreenButton: boolean = true;
-	@Input() showRecordingButton: boolean = true;
-	@Input() showViewRecordingsButton: boolean = false;
-	@Input() showBroadcastingButton: boolean = true;
-	@Input() showBackgroundEffectsButton: boolean = true;
-	@Input() showCaptionsButton: boolean = true;
-	@Input() showSettingsButton: boolean = true;
+	showMoreOptionsButton = input<boolean>(true);
+	showFullscreenButton = input<boolean>(true);
+	showRecordingButton = input<boolean>(true);
+	showViewRecordingsButton = input<boolean>(false);
+	showBroadcastingButton = input<boolean>(true);
+	showBackgroundEffectsButton = input<boolean>(true);
+	showCaptionsButton = input<boolean>(true);
+	showSettingsButton = input<boolean>(true);
 
 	// Fullscreen state
-	@Input() isFullscreenActive: boolean = false;
+	isFullscreenActive = input<boolean>(false);
 
 	// Recording related inputs
-	@Input() recordingStatus: RecordingStatus = RecordingStatus.STOPPED;
-	@Input() hasRoomTracksPublished: boolean = false;
+	recordingStatus = input<RecordingStatus>(RecordingStatus.STOPPED);
+	hasRoomTracksPublished = input<boolean>(false);
 
 	// Broadcasting related inputs
-	@Input() broadcastingStatus: BroadcastingStatus = BroadcastingStatus.STOPPED;
+	broadcastingStatus = input<BroadcastingStatus>(BroadcastingStatus.STOPPED);
 
 	// Captions
-	@Input() captionsEnabled: boolean = false;
+	captionsEnabled = input<boolean>(false);
 
 	// Leave button
-	@Input() showLeaveButton: boolean = true;
+	showLeaveButton = input<boolean>(true);
 
 	// Additional buttons template
-	@Input() toolbarAdditionalButtonsTemplate: TemplateRef<any> | null = null;
-	@Input() additionalButtonsPosition: ToolbarAdditionalButtonsPosition | undefined;
+	toolbarAdditionalButtonsTemplate = input<TemplateRef<any> | null>(null);
+	additionalButtonsPosition = input<ToolbarAdditionalButtonsPosition | undefined>(undefined);
 
 	// Leave button template
-	@Input() toolbarLeaveButtonTemplate: TemplateRef<any> | null = null;
+	toolbarLeaveButtonTemplate = input<TemplateRef<any> | null>(null);
 
 	/**
 	 * @internal
@@ -101,34 +101,34 @@ export class ToolbarMediaButtonsComponent {
 	readonly isDesktopView = computed(() => this.viewportService.isDesktop());
 
 	// Essential buttons that always stay visible
-	readonly showCameraButtonDirect = computed(() => this.showCameraButton && !this.isMinimal);
+	readonly showCameraButtonDirect = computed(() => this.showCameraButton() && !this.isMinimal());
 
-	readonly showMicrophoneButtonDirect = computed(() => this.showMicrophoneButton && !this.isMinimal);
+	readonly showMicrophoneButtonDirect = computed(() => this.showMicrophoneButton() && !this.isMinimal());
 
 	// Screenshare button - visible on tablet+ or when already active
 	readonly showScreenshareButtonDirect = computed(
-		() => this.showScreenshareButton && !this.isMinimal && (!this.isMobileView() || this.isScreenShareEnabled)
+		() => this.showScreenshareButton() && !this.isMinimal() && (!this.isMobileView() || this.isScreenShareEnabled())
 	);
 
 	// More options button - always visible when not minimal
-	readonly showMoreOptionsButtonDirect = computed(() => this.showMoreOptionsButton && !this.isMinimal);
+	readonly showMoreOptionsButtonDirect = computed(() => this.showMoreOptionsButton() && !this.isMinimal());
 
 	// Check if there are active features that should show a badge on More Options
 	readonly hasActiveFeatures = computed(
 		() =>
-			this.isScreenShareEnabled ||
-			this.recordingStatus === this._recordingStatus.STARTED ||
-			this.broadcastingStatus === this._broadcastingStatus.STARTED
+			this.isScreenShareEnabled() ||
+			this.recordingStatus() === this._recordingStatus.STARTED ||
+			this.broadcastingStatus() === this._broadcastingStatus.STARTED
 	);
 
 	// Check if additional buttons should be shown outside (desktop/tablet) or inside More Options (mobile)
 	readonly showAdditionalButtonsOutside = computed(() => {
-		return !this.isMobileView() && this.toolbarAdditionalButtonsTemplate;
+		return !this.isMobileView() && this.toolbarAdditionalButtonsTemplate();
 	});
 
 	// Check if additional buttons should be shown inside More Options menu (mobile only)
 	readonly showAdditionalButtonsInsideMenu = computed(() => {
-		return this.isMobileView() && this.toolbarAdditionalButtonsTemplate;
+		return this.isMobileView() && this.toolbarAdditionalButtonsTemplate();
 	});
 
 	// Media button outputs
